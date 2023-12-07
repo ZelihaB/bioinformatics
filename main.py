6d62d4434619b5171f9c3c86e12ddb3ac4524af8
ZelihaB <71549587+ZelihaB@users.noreply.github.com>
2023-12-07 13:49:24 -0500

    yaml file push

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi('Welcome to Mars')
+    print_hi('Welcome to Mars!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
